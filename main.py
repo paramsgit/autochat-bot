67b08984aafdee215f68a074c7dcbb3073f3f521
Sbsbsv <sbsbsv002@gmail.com>
2023-08-25 21:51:10 +0530

    updating to v2

--- main.py
+++ main.py
@@ -12,6 +12,35 @@ API = os.environ['API']
 def index():
     return render_template('index.html')
 
+# @app.route('/data', methods=['POST'])
+# def get_data():
+    
+#     data = request.get_json()
+#     text=data.get('data')
+#     openai.api_key = API
+    
+#     user_input = text
+#     print(user_input)
+#     try:
+#         response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=user_input,
+#         temperature=0.7,
+#         max_tokens=256,
+#         top_p=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+#         )
+       
+#         model_reply = response['choices'][0]['text']
+#         print(response,model_reply)
+#         return jsonify({"response":True,"message":model_reply})
+#     except Exception as e:
+#         print(e)
+#         error_message = f'Error: {str(e)}'
+#         return jsonify({"message":error_message,"response":False})
+
+
 @app.route('/data', methods=['POST'])
 def get_data():
     
@@ -22,19 +51,8 @@ def get_data():
     user_input = text
     print(user_input)
     try:
-        response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=user_input,
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-        )
-       
-        model_reply = response['choices'][0]['text']
-        print(response,model_reply)
-        return jsonify({"response":True,"message":model_reply})
+        
+        return jsonify({"response":True,"message":"model_reply"})
     except Exception as e:
         print(e)
         error_message = f'Error: {str(e)}'
